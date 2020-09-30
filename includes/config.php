6c4d3be1f39e8a1ef197e51660ffd727cf07bfc7
astratow <davno_pora@mail.com>
2020-09-30 05:41:29 +0100

    css added, db.php added

---
+++ includes/config.php
@@ -0,0 +1,9 @@
+<?php
+
+//user authentification
+$config['salt'] = 'jK7d73';
+
+//error reporting
+mysqli_report(MYSQL_REPORT_ERROR);
+
+?>
---
+++ includes/db.php
@@ -0,0 +1,10 @@
+<?php
+//database settings
+$server= 'localhost';
+$user='root';
+$pass= '';
+$db= 'login';
+//connect to the database
+$mysqli=new mysqli($server, $user, $pass, $db);
+
+?>
