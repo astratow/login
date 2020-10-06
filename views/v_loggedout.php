7c99ec6983c2a09beb5c16a8b200b5eb850232b7
astratow <davno_pora@mail.com>
2020-10-06 06:08:38 +0100

    temp commit 3

--- views/v_loggedout.php
+++ views/v_loggedout.php
@@ -1,9 +1,13 @@
 <html>
 	<head>
 	<title>You have been succesfully logged out!</title>
-	
+	<meta http-equiv="Content-Type" content="text/html" charset="utf-8"/>
+	<meta href="views/style.css" media="screen" rel="stylesheet" type="text/css">
 	</head>
 	
 	<body>
+	<h1>Logged Out</h1>
+	<p>You have been succesfully logged out! You will be redirected to the login page...</p>
+	<noscript> <a href="login.php">Login.php</a> </noscript>
 	</body>
 </html>
