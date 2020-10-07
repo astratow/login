b9999e0079f4f15d52440c59bddd7651cacc72e3
astratow <davno_pora@mail.com>
2020-10-07 05:00:35 +0100

    members.php changed, logout.php created

--- members.php
+++ members.php
@@ -3,6 +3,10 @@
 session_start();
 include("includes/config.php");
 
+//check if user is logged in
+if(!isset($_SESSION['username'])){
+	header("Location: login.php");
+}
 //display view
 include("views/v_members.php");
 ?>
